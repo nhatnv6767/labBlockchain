607fd8575427e762226c2a8289527858fd70e043
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-11-19 23:09:54 +0900

    done 1 1

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,12 @@
 
 module.exports = {
   networks: {
-      development: { //none
+    development: { //none
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
-    },
+  },
 
 
   // Configure your compilers
