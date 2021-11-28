9c167b1807966281f8d12f438dd86bbec78e25dc
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-11-28 20:24:52 +0900

    truffle migrate --reset ( Chuyen va nhan token voi web3js)

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,7 +1,5 @@
 pragma solidity >= 0.4.22 < 0.9.0;
 
 contract Faucet {
-    uint public so_nguyen_duong = 1204012;
-    int public so_nguyen = -1291;
-    address private minter;
+    receive() external payable {}
 }
